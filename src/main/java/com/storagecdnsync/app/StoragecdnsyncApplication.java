cd8a5b85855957fd63d7f7ece9d25a3002eca3de
Mikael Medeiros <mikaeltruth@gmail.com>
2025-11-21 18:09:09 -0300

    first commit

---
+++ src/main/java/com/storagecdnsync/app/StoragecdnsyncApplication.java
@@ -0,0 +1,13 @@
+package com.storagecdnsync.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoragecdnsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoragecdnsyncApplication.class, args);
+	}
+
+}
